c7b972e5439376a726d2216fb01c7aa4f95f1256
Janusz Grabis <janusz@frameworks.pl>
2021-12-20 15:07:57 +0100

    Server cert generation

--- server_cert.pl
+++ server_cert.pl
@@ -1,5 +1,13 @@
 #!/bin/perl
 
-`openssl genrsa -aes256 -out server.key -passout pass:'test' 2048`
+`openssl genrsa -aes256 -out server.key -passout pass:'test' 2048`;
+
+`openssl req -new -key server.key -out server.csr -passin pass:'test' -out server.csr -config server.cnf`;
+
+`openssl x509 -req -days 365 -in server.csr -signkey server.key -passin file:'pass.txt' -out server.crt`;
+
+
+
+
 
 
