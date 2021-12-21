81d9e963583b3cdd4b654bd3fd9707b106164874
Janusz Grabis <janusz@frameworks.pl>
2021-12-21 10:22:58 +0100

    Setting up page for https

--- server_cert.pl
+++ server_cert.pl
@@ -6,8 +6,11 @@
 
 `openssl x509 -req -days 365 -in server.csr -signkey server.key -passin file:'pass.txt' -out server.crt`;
 
+`mkdir -p /etc/nginx/ssl`;
 `cp server.crt /etc/nginx/ssl/server.crt`;
 `cp server.key /etc/nginx/ssl/server.key`;
+`mkdir -p /var/www/ssh_example`;
+`cp index.html /var/www/ssh_example`;
 
 `cp nginx_ssh_config /etc/nginx/sites-available`;
 `cd /etc/nginx/sites-enabled && ln -s ../sites-available/nginx_ssh_config nginx_ssh_config`;
